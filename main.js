b1d025c940c49bba12e4cf7a6e5bf020c77848bb
Rahul Prajapati <raarav717@gmail.com>
2020-06-11 00:34:40 +0530

    load html

---
+++ main.js
@@ -0,0 +1,33 @@
+let fs = require("fs");
+let request = require("request");
+let cheerio = require("cheerio");
+console.log("Sending Request");
+let url = "https://www.monsterindia.com/search/it-computers-software-jobs?searchId=79207d2d-7bea-408b-8d4a-f4bf5cf3e2ad"
+
+request(url,cb);
+function cb(err,response, html){
+    console.log("Recieved Response");
+    if(err == null && response.statusCode == 200){
+        fs.writeFileSync("main.html",html);
+        console.log("File saved");
+        parseHtmlPage(html);
+    }else if(response.statusCode == 404)
+    {
+        console.log("Page not found");
+    }else {
+        console.log(err);
+        console.log(response.statusCode);
+    }
+}
+
+function parseHtmlPage(html){
+    console.log("Parsing Html");
+    let $ = cheerio.load("html");
+    console.log("''''''''''''''''");
+    links = [];
+    let AllJobs = $(".col-md-12").find("data-v-3973e5ee");
+    AllJobs.each(function(){
+        let link = $
+    })
+}
+    
